faa64fe34f1d76d64ea978cffee664cc04fd244c
Martin <mdsugarev@gmail.com>
2022-12-07 13:15:31 +0200

    ref

--- src/Tags.js
+++ src/Tags.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Tags = ({tags}) => {
+export const Tags = ({tags = []}) => {
     return (
              tags.map((t, index) => {
                   return <div key={index} className="tag">{'#' + t}</div>})
